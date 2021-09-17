dabc0b97751330a4a8d83d25d06acb9afa670a50
Marc Haase <marchaase@slingshotaerospace.com>
2021-09-16 17:10:39 -0700

    hook up resolver to DB

--- backend/src/schema/players/PlayerResolver.ts
+++ backend/src/schema/players/PlayerResolver.ts
@@ -1,14 +1,18 @@
 import "reflect-metadata";
-import { Query, Resolver } from "type-graphql";
+import { Ctx, Query, Resolver } from "type-graphql";
 import { Player, PlayersResponse } from "./PlayerResponse";
 import data from "./data";
+import { PlayerEntity } from "./PlayerEntity";
+import { Context } from "../../types/Context";
 
 @Resolver(() => PlayersResponse)
 export class PlayerResolver {
   @Query(() => PlayersResponse)
-  async getPlayers(): Promise<PlayersResponse> {
+  async getPlayers(@Ctx() { em }: Context): Promise<PlayersResponse> {
+    const players = await em.getRepository(PlayerEntity).find();
+
     const playerResponse = new PlayersResponse();
-    playerResponse.players = data;
+    playerResponse.players = players;
     return playerResponse;
   }
 }
