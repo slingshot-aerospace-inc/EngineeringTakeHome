ab3c3460ed4770c005625de7e5006f45a898de05
Marc Haase <marchaase@slingshotaerospace.com>
2021-09-16 17:15:17 -0700

    add better ages

--- backend/src/schema/players/PlayerFactory.ts
+++ backend/src/schema/players/PlayerFactory.ts
@@ -6,7 +6,7 @@ define(PlayerEntity, () => {
   const player = new PlayerEntity();
 
   player.name = `${Faker.name.firstName(0)} ${Faker.name.lastName(0)}`;
-  player.age = Faker.datatype.number(100);
+  player.age = Faker.datatype.number({ min: 18, max: 60 });
   player.position = Faker.random.arrayElements(
     ["LW", "CT", "RW", "ST", "CF", "CAM"],
     Faker.datatype.number({ min: 1, max: 3 })
